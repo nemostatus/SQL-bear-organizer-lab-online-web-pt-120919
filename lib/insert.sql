3694f99d78583a25748f8d69a5fa9cb93a4e8362
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-22 18:11:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO bears(name,age,gender,color,temperament,alive) VALUE
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
 ("Rowdy",1,"male","black","fierce",1);
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
-("Tabitha",3,"female","white","calm",)
+("Tabitha",3,"female","white","calm",1)
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
 ("Sergeant Brown")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
@@ -12,3 +12,4 @@ INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
 ("Grinch")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
 ("Wendy")
+INSERT INTO bears(name,age,gender,color,temperament,alive)
