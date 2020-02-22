a00dc72736ccc94dcd0f9a409e0155ec74a286b3
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-02-22 18:07:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,11 +4,12 @@ INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
 ("Rowdy")
 
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
-
+("Tabitha")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
-
+("")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
-
+("")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
-
+("")
 INSERT INTO bears(name,age,gender,color,temperament,alive) VALUES
+("")
